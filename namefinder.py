517207593788171989acb3408915f6e0f566df80
Seanny123 <saubin@uwaterloo.ca>
2014-09-30 14:51:20 -0400

    trying to send probes and inputs

---
+++ namefinder.py
@@ -0,0 +1,66 @@
+import ipdb
+
+class NameFinder(object):
+    def __init__(self, terms, net):
+        self.base_terms = terms
+        self.known_name = {}
+        for k, v in terms.iteritems():
+            self.known_name[id(v)] = k
+        self.find_names(net)
+
+    def find_names(self, net):
+        """Gets variable names associated to objects. If the object is not assigned to a variable,"""
+        net_name = self.known_name[id(net)]
+
+        base_lists = ['ensembles', 'nodes', 'connections', 'networks', 'probes']
+        ipdb.set_trace()
+        for k in dir(net):
+            # If it's not a private attribute, a built in function and not a Nengo object
+            if not k.startswith('_') and k not in base_lists:
+                # iterate through the attributes of the network that are a list
+                v = getattr(net, k)
+                if isinstance(v, list):
+                    for i, obj in enumerate(v):
+                        # If this object is not already a known name # But then again why would it be?
+                        if not self.known_name.has_key(id(obj)):
+                            # Make the name, the network, the attribute and the index
+                            # This happens for things like connections and other objects
+                            n = '%s.%s[%d]' % (net_name, k, i)
+                            self.known_name[id(obj)] = n
+                            print("Crazy pants 1 %s = %s" %(id(obj), n))
+                else:
+                    # If it's not a list, no need to iterate
+                    self.known_name[id(v)] = '%s.%s' % (net_name, k)
+                    print("Crazy pants 2 %s = %s" %(id(v), self.known_name[id(v)]))
+
+        ipdb.set_trace()
+        for type in base_lists:
+            for i, obj in enumerate(getattr(net, type)):
+                name = self.known_name.get(id(obj), None)
+                print("In base list %s" %(name))
+                # If there was no name found already in the known names
+                if name is None:
+                    name = '%s.%s[%d]' % (net_name, type, i)
+                    self.known_name[id(obj)] = name
+                    print("In base list with no name %s = %s" %(id(obj), name))
+
+        for n in net.networks:
+            self.find_names(n)
+        ipdb.set_trace()
+
+    def name(self, obj):
+        return self.known_name[id(obj)]
+
+
+
+
+if __name__ == '__main__':
+    c = compile(open('scripts/default.py').read(), 'nengo_gui_temp.py', 'exec')
+    locals = {}
+    exec c in globals(), locals
+
+    model = locals['model']
+    ident = NameFinder(locals, model)
+    ident.find_names(locals['model'])
+
+    print ident.known_name
